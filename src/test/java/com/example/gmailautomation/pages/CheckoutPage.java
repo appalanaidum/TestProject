172d06064b5b68aadfe284a4c5c6e747529bcc6c
appalanaidum <manaidu1787@gmail.com>
2024-02-14 15:59:55 +0530

    Made few changes in CheckoutPage class...

--- src/test/java/com/example/gmailautomation/pages/CheckoutPage.java
+++ src/test/java/com/example/gmailautomation/pages/CheckoutPage.java
@@ -1,5 +1,17 @@
 package com.example.gmailautomation.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class CheckoutPage {
-//yet to implement this 
+	private WebDriver driver;
+
+    // Locators
+   // public By usernameInput = By.id("identifierId");
+  //  public By passwordNextButton = By.id("passwordNext");
+
+    public CheckoutPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
 }
