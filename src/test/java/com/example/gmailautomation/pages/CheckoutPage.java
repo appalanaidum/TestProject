5fcccb25d0789c80e7d95336cb3898c5f5199ba5
appalanaidum <manaidu1787@gmail.com>
2024-02-15 17:11:13 +0530

    Jenkins test7 with conflicts...

--- src/test/java/com/example/gmailautomation/pages/CheckoutPage.java
+++ src/test/java/com/example/gmailautomation/pages/CheckoutPage.java
@@ -9,6 +9,11 @@ public class CheckoutPage {
     // Locators
    
     public By passwordNextButton = By.id("passwordNext");
+    public By test1 = By.id("test1");
+    public By test2 = By.name("test2");
+    public By test3 = By.xpath("test3");
+    public By test4 = By.tagName("test4");
+    
 
    public CheckoutPage(WebDriver driver) {
         this.driver = driver;
