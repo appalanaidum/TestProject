4bb7455994516cebaf2f96f83e4b3a11b98dcad6
appalanaidum <manaidu1787@gmail.com>
2024-02-15 16:00:43 +0530

    Jenkins test3...

--- src/test/java/com/example/gmailautomation/pages/CheckoutPage.java
+++ src/test/java/com/example/gmailautomation/pages/CheckoutPage.java
@@ -7,8 +7,8 @@ public class CheckoutPage {
 	private WebDriver driver;
 
     // Locators
-    public By usernameInput = By.id("identifierId");
-    public By passwordNextButton = By.id("passwordNext");
+   // public By usernameInput = By.id("identifierId");
+   // public By passwordNextButton = By.id("passwordNext");
 
    public CheckoutPage(WebDriver driver) {
         this.driver = driver;
