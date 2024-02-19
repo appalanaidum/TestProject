fd715216b5f0bd871b5b725823acde3e951dd9b4
appalanaidum <manaidu1787@gmail.com>
2024-02-19 16:37:09 +0530

    Removed few webelements from CheckoutPage Calss...

--- src/test/java/com/example/gmailautomation/pages/CheckoutPage.java
+++ src/test/java/com/example/gmailautomation/pages/CheckoutPage.java
@@ -6,30 +6,17 @@ import org.openqa.selenium.WebDriver;
 public class CheckoutPage {
 	private WebDriver driver;
 
-    // Locators
-   
-    public By passwordNextButton = By.id("passwordNext");
+	// Locators
 
+	public By passwordNextButton = By.id("passwordNext");
 
-    
-    public By test5 = By.xpath("test");
-    public By test6 = By.tagName("test");
-    public By test7 = By.id("test1");
-    public By test8 = By.name("test2");
-    public By test9 = By.tagName("test9");
-    public By test10 = By.tagName("test10");
-    public By test1 = By.id("test");
-    public By test2 = By.name("test");
-    public By test3 = By.xpath("test3");
-    public By test4 = By.tagName("test4");
+	public By test1 = By.id("test");
+	public By test2 = By.name("test");
+	public By test3 = By.xpath("test3");
+	public By test4 = By.tagName("test4");
 
-    public By test11 = By.xpath("test11");
-    public By test12 = By.tagName("test12");
-    public By test13 = By.tagName("test13");
-    public By test14 = By.tagName("test14");
-    public By test15 = By.tagName("test15");
-   public CheckoutPage(WebDriver driver) {
-        this.driver = driver;
-    }
+	public CheckoutPage(WebDriver driver) {
+		this.driver = driver;
+	}
 //test
 }
